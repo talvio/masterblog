ebc8fed92a2eac8bf67be96da35173923df599a6
Jukka Talvio <jukka.talvio@iki.fi>
2025-03-20 13:14:56 +0100

    Now accepting new blog posts

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import blog_posts
 
 app = Flask(__name__)
@@ -8,6 +8,16 @@ def index():
     blogs = blog_posts.get_blogs()
     return render_template('index.html', posts=blogs)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form.get('title', "")
+        author = request.form.get('author',"")
+        content = request.form.get('content', "")
+        blog_posts.add_blog(title, author, content)
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
--- blog_posts.py
+++ blog_posts.py
@@ -13,7 +13,34 @@ def get_blogs(blog_file=BLOG_FILE):
     ]
     """
     if blog_file is None:
-        blog_file = self.blog_file
+        blog_file = BLOG_FILE
     with open(blog_file, 'r') as json_file:
         blogs = json.load(json_file)
     return blogs
+
+def save_blogs(blogs, blog_file=BLOG_FILE):
+    """
+    Saves blogs to a file
+    """
+    if blog_file is None:
+        blog_file = BLOG_FILE
+    with open(blog_file, 'w') as json_file:
+        json.dump(blogs, json_file)
+
+def add_blog(title, author, content):
+    """
+    Add a new blog post
+    """
+    blogs = get_blogs()
+    new_blog_id = str(max(int(blog.get('id', 0)) + 1 for blog in blogs ))
+    blogs.append(
+        {
+            'id': new_blog_id,
+            'title': title,
+            'author': author,
+            'content': content,
+        }
+    )
+    save_blogs(blogs)
+    return new_blog_id
+
